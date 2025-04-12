fc3354ca339fc512f99a22d91b8a2f5a8cdf7406
awquari <marii.mishina@yandex.ru>
2025-04-12 20:18:09 +0300

    HTMLParser realized

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -2,16 +2,64 @@
 Crawler implementation.
 """
 
+import datetime
+import json
+
 # pylint: disable=too-many-arguments, too-many-instance-attributes, unused-import, undefined-variable, unused-argument
 import pathlib
-import json
-from exceptions import *
-import requests
-import shutil
 import re
+import shutil
 from typing import Pattern, Union
+
+import requests
+from bs4 import BeautifulSoup
+
+from core_utils.article.article import Article
+from core_utils.article.io import to_raw
 from core_utils.config_dto import ConfigDTO
-from core_utils.constants import CRAWLER_CONFIG_PATH, ASSETS_PATH
+from core_utils.constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
+
+
+class IncorrectSeedURLError(Exception):
+    """
+     Raising an error when a seed URL does not match standard pattern "https?://(www.)?"
+    """
+
+
+class NumberOfArticlesOutOfRangeError(Exception):
+    """
+     Raising an error when total number of articles is out of range from 1 to 150
+    """
+
+
+class IncorrectNumberOfArticlesError(Exception):
+    """
+     Raising an error when total number of articles to parse is not integer or less than 0
+    """
+
+
+class IncorrectHeadersError(Exception):
+    """
+     Raising an error when headers are not in a form of dictionary
+    """
+
+
+class IncorrectEncodingError(Exception):
+    """
+     Raising an error when encoding is not specified as a string
+    """
+
+
+class IncorrectTimeoutError(Exception):
+    """
+     Raising an error when timeout value is not a positive integer less than 60
+    """
+
+
+class IncorrectVerifyError(Exception):
+    """
+     Raising an error when verify certificate value is not True or False
+    """
 
 
 class Config:
@@ -154,108 +202,154 @@ def make_request(url: str, config: Config) -> requests.models.Response:
                             headers=config.get_headers(),
                             timeout=config.get_timeout(),
                             verify=config.get_verify_certificate())
-    response.encoding = config.get_encoding()
     return response
 
 
-#
-#
-# class Crawler:
-#     """
-#     Crawler implementation.
-#     """
-#
-#     #: Url pattern
-#     url_pattern: Union[Pattern, str]
-#
-#     def __init__(self, config: Config) -> None:
-#         """
-#         Initialize an instance of the Crawler class.
-#
-#         Args:
-#             config (Config): Configuration
-#         """
-#
-#     def _extract_url(self, article_bs: BeautifulSoup) -> str:
-#         """
-#         Find and retrieve url from HTML.
-#
-#         Args:
-#             article_bs (bs4.BeautifulSoup): BeautifulSoup instance
-#
-#         Returns:
-#             str: Url from HTML
-#         """
-#
-#     def find_articles(self) -> None:
-#         """
-#         Find articles.
-#         """
-#
-#     def get_search_urls(self) -> list:
-#         """
-#         Get seed_urls param.
-#
-#         Returns:
-#             list: seed_urls param
-#         """
-#
-#
+class Crawler:
+    """
+    Crawler implementation.
+    """
+
+    #: Url pattern
+    url_pattern: Union[Pattern, str]
+
+    def __init__(self, config: Config) -> None:
+        """
+        Initialize an instance of the Crawler class.
+
+        Args:
+            config (Config): Configuration
+        """
+        self.config = config
+        self.urls = []
+
+    def _extract_url(self, article_bs: BeautifulSoup) -> str:
+        """
+        Find and retrieve url from HTML.
+
+        Args:
+            article_bs (bs4.BeautifulSoup): BeautifulSoup instance
+
+        Returns:
+            str: Url from HTML
+        """
+        for title_tag in article_bs.find_all("h3", class_="news-title"):
+            link_tag = title_tag.find("a", href=True)
+            if link_tag:
+                href = link_tag["href"]
+                if not href.startswith("/news"):
+                    continue
+                full_url = "https://www.baikal-daily.ru" + str(href)
+                if full_url and full_url not in self.urls:
+                    return full_url
+        return ""
+
+    def find_articles(self) -> None:
+        """
+        Find articles.
+        """
+        for seed_url in self.config.get_seed_urls():
+            if len(self.urls) == self.config.get_num_articles():
+                break
+
+            try:
+                response = make_request(url=seed_url, config=self.config)
+                response.raise_for_status()
+            except requests.HTTPError:
+                continue
+
+            page_bs = BeautifulSoup(response.text, 'html.parser')
+
+            extracted_url = self._extract_url(page_bs)
+            while extracted_url:
+                self.urls.append(extracted_url)
+                if len(self.urls) == self.config.get_num_articles():
+                    break
+                extracted_url = self._extract_url(page_bs)
+
+    def get_search_urls(self) -> list:
+        """
+        Get seed_urls param.
+
+        Returns:
+            list: seed_urls param
+        """
+        return self.config.get_seed_urls()
+
+
 # # 10
 # # 4, 6, 8, 10
-#
-#
-# class HTMLParser:
-#     """
-#     HTMLParser implementation.
-#     """
-#
-#     def __init__(self, full_url: str, article_id: int, config: Config) -> None:
-#         """
-#         Initialize an instance of the HTMLParser class.
-#
-#         Args:
-#             full_url (str): Site url
-#             article_id (int): Article id
-#             config (Config): Configuration
-#         """
-#
-#     def _fill_article_with_text(self, article_soup: BeautifulSoup) -> None:
-#         """
-#         Find text of article.
-#
-#         Args:
-#             article_soup (bs4.BeautifulSoup): BeautifulSoup instance
-#         """
-#
-#     def _fill_article_with_meta_information(self, article_soup: BeautifulSoup) -> None:
-#         """
-#         Find meta information of article.
-#
-#         Args:
-#             article_soup (bs4.BeautifulSoup): BeautifulSoup instance
-#         """
-#
-#     def unify_date_format(self, date_str: str) -> datetime.datetime:
-#         """
-#         Unify date format.
-#
-#         Args:
-#             date_str (str): Date in text format
-#
-#         Returns:
-#             datetime.datetime: Datetime object
-#         """
-#
-#     def parse(self) -> Union[Article, bool, list]:
-#         """
-#         Parse each article.
-#
-#         Returns:
-#             Union[Article, bool, list]: Article instance
-#         """
-#
-#
+
+
+class HTMLParser:
+    """
+    HTMLParser implementation.
+    """
+
+    def __init__(self, full_url: str, article_id: int, config: Config) -> None:
+        """
+        Initialize an instance of the HTMLParser class.
+
+        Args:
+            full_url (str): Site url
+            article_id (int): Article id
+            config (Config): Configuration
+        """
+        self.full_url = full_url
+        self.article_id = article_id
+        self.config = config
+        self.article = Article(url=full_url, article_id=article_id)
+
+    def _fill_article_with_text(self, article_soup: BeautifulSoup) -> None:
+        """
+        Find text of article.
+
+        Args:
+            article_soup (bs4.BeautifulSoup): BeautifulSoup instance
+        """
+        article_div = article_soup.find("div", class_="news-text")
+        text = []
+        for element in article_div:
+            if element.get_text().strip():
+                text.append(element.get_text(strip=True, separator="\n"))
+        self.article.text = "\n".join(text)
+
+    def _fill_article_with_meta_information(self, article_soup: BeautifulSoup) -> None:
+        """
+        Find meta information of article.
+
+        Args:
+            article_soup (bs4.BeautifulSoup): BeautifulSoup instance
+        """
+        pass
+
+    def unify_date_format(self, date_str: str) -> datetime.datetime:
+        """
+        Unify date format.
+
+        Args:
+            date_str (str): Date in text format
+
+        Returns:
+            datetime.datetime: Datetime object
+        """
+        pass
+
+    def parse(self) -> Union[Article, bool, list]:
+        """
+        Parse each article.
+
+        Returns:
+            Union[Article, bool, list]: Article instance
+        """
+        response = make_request(self.full_url, self.config)
+        if response.ok:
+            article_bs = BeautifulSoup(response.text, 'html.parser')
+            self._fill_article_with_text(article_bs)
+            self._fill_article_with_meta_information(article_bs)
+        return self.article
+
+
 def prepare_environment(base_path: Union[pathlib.Path, str]) -> None:
     """
     Create ASSETS_PATH folder if no created and remove existing folder.
@@ -274,7 +368,10 @@ def main() -> None:
     """
     configuration = Config(path_to_config=CRAWLER_CONFIG_PATH)
     prepare_environment(ASSETS_PATH)
-    print(configuration.get_num_articles())
+    crawler = Crawler(config=configuration)
+    crawler.find_articles()
+    parser = HTMLParser(crawler.urls[0], 1, configuration)
+    to_raw(parser.parse())
 
 
 if __name__ == "__main__":
