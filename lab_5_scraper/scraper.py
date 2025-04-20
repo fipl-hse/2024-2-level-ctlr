773e223977db61e1e5b1785d689c4ff9398b57d6
tasya-kakoulina <kaktotaksus@yandex.ru>
2025-04-20 09:38:40 +0300

    small changes

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -366,11 +366,11 @@ def prepare_environment(base_path: Union[pathlib.Path, str]) -> None:
         base_path (Union[pathlib.Path, str]): Path where articles stores
     """
     base_path = pathlib.Path(base_path)
-    if base_path.mkdir(exist_ok=True):
+    if base_path.mkdir(exist_ok=True, parents=True):
         return None
     if any(base_path.iterdir()):
         shutil.rmtree(base_path)
-        base_path.mkdir()
+        base_path.mkdir(parents=True)
 
 
 class CrawlerRecursive(Crawler):
