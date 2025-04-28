c3b79111a9c2bf85690d10aa6caae85759099bee
SabirovaLilia <Lilia20s05@yandex.ru>
2025-04-28 17:56:15 +0300

    changes in imports

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -4,17 +4,19 @@ Crawler implementation.
 
 import datetime
 import json
+
 # pylint: disable=too-many-arguments, too-many-instance-attributes, unused-import, undefined-variable, unused-argument
 import pathlib
 import shutil
 from typing import Pattern, Union
+
 import requests
 from bs4 import BeautifulSoup
+
 from core_utils.article.article import Article
 from core_utils.article.io import to_meta, to_raw
-from core_utils.constants import ASSETS_PATH
-from core_utils.constants import CRAWLER_CONFIG_PATH
 from core_utils.config_dto import ConfigDTO
+from core_utils.constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
 
 
 class IncorrectSeedURLError(Exception):
