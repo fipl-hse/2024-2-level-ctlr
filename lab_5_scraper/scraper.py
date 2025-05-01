93b1d1f3cd1bba900569cc1b4bcf5a387afb727e
KolkiRik <2005kit2005@mail.ru>
2025-05-02 00:24:42 +0300

    ?

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -200,7 +200,7 @@ def make_request(url: str, config: Config) -> requests.models.Response:
     Returns:
         requests.models.Response: A response from a request
     """
-    sleep(15)
+    sleep(10)
     response = requests.get(
         url,
         headers=config.get_headers(),
