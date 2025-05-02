51113b58376d08bf59293d694767852b125ba96f
evgraff2005 <annaevgrafova22095@gmail.com>
2025-05-02 10:10:55 +0300

    ...

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -341,9 +341,15 @@ class HTMLParser:
                 self.article.author = ([article_author.get_text(strip=True)]
                                        if article_author else ["NOT FOUND"])
 
-            date = div.find('meta', {'property': 'article:published_time'})
-            self.article.date = (self.unify_date_format(date['content'])
-                                 if date else datetime.datetime.now().replace(microsecond=0))
+            date_meta = div.find('meta', {'property': 'article:published_time'})
+            if date_meta and date_meta.get('content'):
+                content = str(date_meta['content'])
+                if content:
+                    self.article.date = self.unify_date_format(content)
+                else:
+                    self.article.date = datetime.datetime.now().replace(microsecond=0)
+            else:
+                self.article.date = datetime.datetime.now().replace(microsecond=0)
 
             self.article.topics = []
 
