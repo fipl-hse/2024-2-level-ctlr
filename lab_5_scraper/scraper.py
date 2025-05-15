670c942b67dbbae11ca452018737cc37d8ef5b37
Olya <okot30652@gmail.com>
2025-05-15 14:00:16 +0300

    aaa

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -99,9 +99,9 @@ class Config:
         if not all(url.startswith('https://pravdasevera.ru/') for url in self.seed_urls):
             raise IncorrectSeedURLError("Each seed URL must start with 'https://pravdasevera.ru/'")
 
-        if not isinstance(self.num_articles, int) or not (0 < self.num_articles <= 60):
+        if not isinstance(self.num_articles, int) or not (0 <= self.num_articles <= 60):
             raise IncorrectNumberOfArticlesError(
-                "Number of articles must be an integer between 0 and 60. 0 is a valid value.")
+                "Number of articles must be an integer between 0 and 60. ")
 
         if not isinstance(self.encoding, str):
             raise IncorrectEncodingError("Encoding must be a string.")
@@ -123,7 +123,7 @@ class Config:
         if not isinstance(self.num_articles, int):
             raise IncorrectNumberOfArticlesError("Number of articles must be an integer.")
 
-        if self.num_articles <= 0 or self.num_articles > 61:
+        if self.num_articles <= 1 or self.num_articles > 61:
             raise NumberOfArticlesOutOfRangeError("Number of articles must be between 1 and 60.")
 
     def get_seed_urls(self) -> list[str]:
