b47f0da73aae72d6a9ecf7b055f67bea0d7bd427
KolkiRik <2005Kit2005@mail.ru>
2025-05-16 14:37:56 +0300

    oops

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -325,13 +325,13 @@ class HTMLParser:
                           .text)["@graph"][1]['datePublished']
         self.article.date = self.unify_date_format(date)
         if 'https://tuvapravda.ru/natsionalnye-proekty/' in self.article.url:
-            self.article.topics = 'Национальные проекты'
+            self.article.topics = ['Национальные проекты']
         elif 'https://tuvapravda.ru/novosti/' in self.article.url:
-            self.article.topics = 'Новости'
+            self.article.topics = ['Новости']
         elif 'https://tuvapravda.ru/fotofakt/' in self.article.url:
-            self.article.topics = 'Фотофакт'
+            self.article.topics = ['Фотофакт']
         else:
-            self.article.topics = 'NOT FOUND'
+            self.article.topics = ['NOT FOUND']
         self.article.author = [article_soup.find("span", class_="author__name").text]
 
     def unify_date_format(self, date_str: str) -> datetime.datetime:
