0373e5cbe30643cd08308532162150966440ac20
CloudWhisp <sinsinitsa2024@gmail.com>
2025-05-18 19:07:54 +0300

    1. mark 4 code done

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -4,10 +4,13 @@ Pipeline for CONLL-U formatting.
 
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-nested-blocks
 import pathlib
+import os
+import re
 
 from networkx import DiGraph
 
 from core_utils.article.article import Article
+from core_utils.article.io import to_cleaned
 from core_utils.pipeline import (
     AbstractCoNLLUAnalyzer,
     CoNLLUDocument,
@@ -18,7 +21,16 @@ from core_utils.pipeline import (
     UDPipeDocument,
     UnifiedCoNLLUDocument,
 )
+from core_utils.constants import ASSETS_PATH
 
+class InconsistentDatasetError(Exception):
+    '''Data in directory is inconsistent'''
+
+class EmptyDirectoryError(Exception):
+    '''Directory is empty'''
+
+class EmptyFileError(Exception):
+    ''''''
 
 class CorpusManager:
     """
@@ -32,16 +44,63 @@ class CorpusManager:
         Args:
             path_to_raw_txt_data (pathlib.Path): Path to raw txt data
         """
+        self.path_to_raw_txt_data = path_to_raw_txt_data
+        self._storage = {}
+        self._validate_dataset()
+        self._scan_dataset()
 
     def _validate_dataset(self) -> None:
         """
         Validate folder with assets.
         """
 
+        if not self.path_to_raw_txt_data.exists():
+            raise FileNotFoundError("File not found")
+
+        if not self.path_to_raw_txt_data.is_dir():
+            raise NotADirectoryError("Path leads to something other than Directory")
+
+        files_list = list(map(str, self.path_to_raw_txt_data.iterdir()))
+        if len(files_list) == 0:
+            raise EmptyDirectoryError("Directory is empty")
+
+        for file in self.path_to_raw_txt_data.iterdir():
+            if os.stat(file).st_size == 0:
+                raise InconsistentDatasetError("One of the files is empty")
+
+        json_count = [file for file in files_list if '.json' in file]
+        json_count = sorted(json_count, key=lambda m: int(m.split("\\")[-1].split('_')[0]))
+        article_count = [file for file in files_list if 'raw.txt' in file]
+        article_count = sorted(article_count, key=lambda m: int(m.split("\\")[-1].split('_')[0]))
+
+        if len(json_count) != len(article_count):
+            raise InconsistentDatasetError("Number of meta files doesn't match the number of articles")
+
+        for file_id, file_name in enumerate(article_count):
+            if file_name.startswith(str(file_id)) and json_count[file_id].startswith(str(file_id)):
+                raise InconsistentDatasetError('Slip ups in ID detected')
+
+        return None
+
     def _scan_dataset(self) -> None:
         """
         Register each dataset entry.
         """
+        files_list = list(map(str, self.path_to_raw_txt_data.iterdir()))
+        files_new = []
+        for file in files_list:
+            if file.endswith('raw.txt'):
+                files_new.append(file)
+        files_new = sorted(files_new, key=lambda m: int(m.split("\\")[-1].split('_')[0]))
+        iter_count = 0
+        for file_id, file in enumerate(files_new):
+            if str(file).endswith('raw.txt'):
+                f = open(file, 'r', encoding="UTF-8")
+                iter_count += 1
+                article = Article(url=None, article_id=iter_count)
+                article.text = f.read()
+                self._storage[iter_count] = article
+                f.close()
 
     def get_articles(self) -> dict:
         """
@@ -50,7 +109,7 @@ class CorpusManager:
         Returns:
             dict: Storage params
         """
-
+        return self._storage
 
 class TextProcessingPipeline(PipelineProtocol):
     """
@@ -67,12 +126,18 @@ class TextProcessingPipeline(PipelineProtocol):
             corpus_manager (CorpusManager): CorpusManager instance
             analyzer (LibraryWrapper | None): Analyzer instance
         """
-
+        self.corpus_manager = corpus_manager
+        self.analyzer = analyzer
     def run(self) -> None:
         """
         Perform basic preprocessing and write processed text to files.
         """
-
+        articles = self.corpus_manager.get_articles()
+        for _, article in articles.items():
+            article.text = article.text.lower()
+            article.text = re.sub(r'[^\w\s]', '', article.text)
+            article.text = re.sub(r' ', '', article.text)
+            to_cleaned(article)
 
 class UDPipeAnalyzer(LibraryWrapper):
     """
@@ -293,7 +358,9 @@ def main() -> None:
     """
     Entrypoint for pipeline module.
     """
-
+    corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
+    pipeline = TextProcessingPipeline(corpus_manager)
+    pipeline.run()
 
 if __name__ == "__main__":
     main()
