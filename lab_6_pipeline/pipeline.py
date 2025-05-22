99b6c761214e54409e1a513fe2f1cea8a24b9bae
dkole13 <dasha10206@gmail.com>
2025-05-23 00:26:19 +0300

    final changes

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -5,10 +5,12 @@ Pipeline for CONLL-U formatting.
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-nested-blocks
 import pathlib
 from collections import defaultdict
+from dataclasses import asdict
 
 import spacy_udpipe
 import stanza
-from networkx import DiGraph
+from networkx import DiGraph, to_dict_of_lists
+from networkx.algorithms.isomorphism import categorical_node_match, GraphMatcher
 from spacy_conll.parser import ConllParser
 from stanza.models.common.doc import Document
 from stanza.utils.conll import CoNLL
@@ -70,11 +72,11 @@ class CorpusManager:
         Validate folder with assets.
         """
         if not self.path.exists():
-            raise FileNotFoundError
+            raise FileNotFoundError(f'File {self.path} does not exist.')
         if not self.path.is_dir():
-            raise NotADirectoryError
+            raise NotADirectoryError(f'{self.path} is not a directory.')
         if not any(self.path.iterdir()):
-            raise EmptyDirectoryError
+            raise EmptyDirectoryError(f'Directory {self.path} is empty.')
         meta = [filepath.name for filepath in self.path.glob('*_meta.json')]
         raw = [filepath.name for filepath in self.path.glob('*_raw.txt')]
         if len(meta) != len(raw):
@@ -87,9 +89,8 @@ class CorpusManager:
         if set(raw) != set(raw_template):
             raise InconsistentDatasetError('IDs of raw files are inconsistent.')
         for mask in ['*_raw.txt', '*_meta.json']:
-            for filepath in self.path.glob(mask):
-                if filepath.stat().st_size == 0:
-                    raise InconsistentDatasetError(f'File {filepath} is empty.')
+            if any(filepath.stat().st_size == 0 for filepath in self.path.glob(mask)):
+                raise InconsistentDatasetError('Some files are empty.')
 
     def _scan_dataset(self) -> None:
         """
@@ -114,7 +115,7 @@ class TextProcessingPipeline(PipelineProtocol):
     """
 
     def __init__(
-            self, corpus_manager: CorpusManager, analyzer: LibraryWrapper | None = None
+        self, corpus_manager: CorpusManager, analyzer: LibraryWrapper | None = None
     ) -> None:
         """
         Initialize an instance of the TextProcessingPipeline class.
@@ -163,7 +164,7 @@ class UDPipeAnalyzer(LibraryWrapper):
         model_path = pathlib.Path(PROJECT_ROOT) /"lab_6_pipeline" /\
                      "assets" / "model" / "russian-syntagrus-ud-2.0-170801.udpipe"
         if not model_path.exists():
-            raise FileNotFoundError("Path to model does not exists or is invalid")
+            raise FileNotFoundError("Path to model does not exists or is invalid.")
         model = spacy_udpipe.load_from_path(
             lang='ru',
             path=str(model_path)
@@ -210,7 +211,7 @@ class UDPipeAnalyzer(LibraryWrapper):
         """
         conllu_path = article.get_file_path(ArtifactType.UDPIPE_CONLLU)
         if pathlib.Path(conllu_path).stat().st_size == 0:
-            raise EmptyFileError
+            raise EmptyFileError(f'File {conllu_path} is empty.')
         with open(conllu_path, encoding='utf-8') as file:
             conllu = file.read()
         parsed_conllu: UDPipeDocument = ConllParser(
@@ -241,6 +242,7 @@ class StanzaAnalyzer(LibraryWrapper):
         """
         Initialize an instance of the StanzaAnalyzer class.
         """
+        self._analyzer = self._bootstrap()
 
     def _bootstrap(self) -> AbstractCoNLLUAnalyzer:
         """
@@ -289,9 +291,11 @@ class StanzaAnalyzer(LibraryWrapper):
         Returns:
             StanzaDocument: Document ready for parsing
         """
-        conllu_path = article.get_file_path(ArtifactType.UDPIPE_CONLLU)
+        conllu_path = article.get_file_path(ArtifactType.STANZA_CONLLU)
+        if not pathlib.Path(conllu_path).exists():
+            raise FileNotFoundError(f'File {conllu_path} does not exist.')
         if pathlib.Path(conllu_path).stat().st_size == 0:
-            raise EmptyFileError
+            raise EmptyFileError(f'File {conllu_path} is empty.')
         parsed_conllu: StanzaDocument = CoNLL.conll2doc(conllu_path)
         return parsed_conllu
 
@@ -335,8 +339,12 @@ class POSFrequencyPipeline:
         """
         article_conllu = self._analyzer.from_conllu(article)
         pos_frequencies = defaultdict(int)
-        for token in article_conllu:
-            pos_frequencies[token.pos_] += 1
+        if isinstance(self._analyzer, UDPipeAnalyzer):
+            for token in article_conllu:
+                pos_frequencies[token.pos_] += 1
+        else:
+            for word in article_conllu.iter_words():
+                pos_frequencies[word.upos] += 1
         return pos_frequencies
 
     def run(self) -> None:
@@ -344,7 +352,9 @@ class POSFrequencyPipeline:
         Visualize the frequencies of each part of speech.
         """
         for idx, article in self._corpus.get_articles().items():
-            from_meta(article.get_meta_file_path(), article)
+            if pathlib.Path(meta_path := article.get_meta_file_path()).stat().st_size == 0:
+                raise EmptyFileError(f'File {meta_path} is empty.')
+            from_meta(meta_path, article)
             article.set_pos_info(self._count_frequencies(article))
             to_meta(article)
             visualize(article, pathlib.Path(ASSETS_PATH) / f"{idx}_image.png")
@@ -356,7 +366,7 @@ class PatternSearchPipeline(PipelineProtocol):
     """
 
     def __init__(
-            self, corpus_manager: CorpusManager, analyzer: LibraryWrapper, pos: tuple[str, ...]
+        self, corpus_manager: CorpusManager, analyzer: LibraryWrapper, pos: tuple[str, ...]
     ) -> None:
         """
         Initialize an instance of the PatternSearchPipeline class.
@@ -381,17 +391,25 @@ class PatternSearchPipeline(PipelineProtocol):
             list[DiGraph]: Graphs for the sentences in the document
         """
         graphs = []
+        if isinstance(doc, Document):
+            for sentence in doc.sentences:
+                graph = DiGraph()
+                for word in sentence.words:
+                    graph.add_node(word.id, label=word.upos)
+                    graph.add_edge(word.head, word.id, label=word.deprel)
+                graphs.append(graph)
+            return graphs
         for sentence in doc.sents:
             graph = DiGraph()
-            for word in sentence.words:
-                word = word.to_dict()
-                graph.add_node(word['id'], label=word['upos'])
-                graph.add_edge(word['head'], word['id'], label=word['deprel'])
+            for word in sentence:
+                graph.add_node(word.i, label=word.pos_)
+                graph.add_edge(word.head, word.i, label=word.dep_)
             graphs.append(graph)
         return graphs
 
+
     def _add_children(
-            self, graph: DiGraph, subgraph_to_graph: dict, node_id: int, tree_node: TreeNode
+        self, graph: DiGraph, subgraph_to_graph: dict, node_id: int, tree_node: TreeNode
     ) -> None:
         """
         Add children to TreeNode.
@@ -421,18 +439,51 @@ class PatternSearchPipeline(PipelineProtocol):
         Returns:
             dict[int, list[TreeNode]]: A dictionary with pattern matches
         """
+        matches = {}
+        pattern_graph = DiGraph()
+        pattern_graph.add_nodes_from((idx, {"label": label})
+                                     for idx, label in enumerate(self._node_labels))
+        for sentence_id, graph in enumerate(doc_graphs):
+            matcher = GraphMatcher(graph, pattern_graph,
+                                   node_match=categorical_node_match('label', ''))
+            pattern_nodes = []
+
+            for match in matcher.subgraph_isomorphisms_iter():
+                isomorphic_graph = graph.subgraph(match.keys()).copy()
+                for node in isomorphic_graph.nodes():
+                    tree_node = TreeNode(isomorphic_graph.nodes[node].get('label'),
+                                         isomorphic_graph.nodes[node].get('text'),
+                                         [])
+                    self._add_children(
+                        graph, to_dict_of_lists(pattern_graph), node, tree_node
+                    )
+                    pattern_nodes.append(tree_node)
+
+            if pattern_nodes:
+                matches[sentence_id] = pattern_nodes
+        return matches
 
     def run(self) -> None:
         """
         Search for a pattern in documents and writes found information to JSON file.
         """
+        for article in list(self._corpus.get_articles().values())[:1]:
+            article_conllu = self._analyzer.from_conllu(article)
+            article_graphs = self._make_graphs(article_conllu)
+            article_matches = self._find_pattern(article_graphs)
+            article_matches_dict = {
+                idx: [asdict(m) for m in article_match]
+                for idx, article_match in article_matches.items()
+            }
+            article.set_patterns_info(article_matches_dict)
+            to_meta(article)
 
 
 def main() -> None:
     """
     Entrypoint for pipeline module.
     """
-    path = pathlib.Path(__file__).parent.parent / "tmp" / "articles"
+    path = pathlib.Path(ASSETS_PATH)
     corpus_manager = CorpusManager(path)
     udpipe_analyzer = UDPipeAnalyzer()
 
