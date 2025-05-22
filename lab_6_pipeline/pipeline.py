498242e3406536f5ec14cb8ec6ce6e31563981dd
unknown <rjkzye2@mail.ru>
2025-05-22 20:12:32 +0300

    .

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -7,7 +7,7 @@ import pathlib
 
 import spacy_udpipe
 from networkx import DiGraph
-from spacy_conll import ConllParser  # type: ignore[import-not-found]
+from spacy_conll import ConllParser  # type: ignore[import-not-found, import-untyped]
 
 from core_utils.article.article import Article, ArtifactType
 from core_utils.article.io import from_raw, to_cleaned
