ac891698e846a71942f04edd94171559c542bdae
minabdeyeva <olivia.aminel@yandex.ru>
2025-05-22 19:03:40 +0300

    code style SEMINARS fix 4

--- seminars/seminar_05_16_2025/try_udpipe.py
+++ seminars/seminar_05_16_2025/try_udpipe.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 try:
     import spacy
-    import spacy_undpipe
+    import spacy_udpipe
     from spacy_conll import ConllParser  # type: ignore[import-not-found, import-untyped]
 
 except ImportError:
