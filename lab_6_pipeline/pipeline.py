1bfde433929a79d7a0ffe16bc69f2ec7a7d07791
unknown <rjkzye2@mail.ru>
2025-05-22 19:05:32 +0300

    import style error

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -4,13 +4,12 @@ Pipeline for CONLL-U formatting.
 
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-nested-blocks
 import pathlib
-import spacy_udpipe
 
+import spacy_udpipe
 from networkx import DiGraph
 
-from core_utils.article.io import to_cleaned
-from core_utils.article.io import from_raw
 from core_utils.article.article import Article, ArtifactType
+from core_utils.article.io import from_raw, to_cleaned
 from core_utils.constants import ASSETS_PATH, PROJECT_ROOT
 from core_utils.pipeline import (
     AbstractCoNLLUAnalyzer,
