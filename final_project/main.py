458fd0e7ae9100b73af75f6318717d7979d80193
QuietHellsPage <baazhinga@yandex.ru>
2025-06-04 19:53:14 +0300

    anone

--- final_project/main.py
+++ final_project/main.py
@@ -13,25 +13,24 @@ def main() -> None:
     Generate conllu file for provided corpus of texts.
     """
     project_dir = Path(__file__).parent
-
     assets_dir = project_dir / "assets/blok"
     output_dir = project_dir / "dist"
     output_file = output_dir / "auto_annotated.conllu"
 
     analyzer = UDPipeAnalyzer()
-    all_conllu = []
+    all_conllu: list[str] = []
 
     for txt in assets_dir.glob("*.txt"):
         with open(txt, "r", encoding="utf-8") as file:
             text = file.read().strip()
 
         conllu_data = analyzer.analyze([text])[0]
-        all_conllu.append(conllu_data)
+        all_conllu.append(str(conllu_data).strip())
 
     output_dir.mkdir(parents=True, exist_ok=True)
 
     with open(output_file, "w", encoding="utf-8") as file:
-        file.write("\n\n".join(all_conllu))
+        file.write("\n\n".join(all_conllu) + "\n")
 
 if __name__ == "__main__":
     main()
