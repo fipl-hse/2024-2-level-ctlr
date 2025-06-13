837b23415a3510fae1d2154f82a7af81c6ee1ac5
tasya-kakoulina <kaktotaksus@yandex.ru>
2025-06-13 17:37:37 +0300

    conllu data retrieve

--- final_project/main.py
+++ final_project/main.py
@@ -4,12 +4,30 @@ Final project implementation.
 
 # pylint: disable=unused-import
 from pathlib import Path
+from core_utils.constants import PROJECT_ROOT
+from lab_6_pipeline.pipeline import UDPipeAnalyzer
 
 
 def main() -> None:
     """
     Generate conllu file for provided corpus of texts.
     """
+    path = PROJECT_ROOT / 'final_project'
+    assets_path = path / 'assets' / 'pasternak'
+    dist_path = path / 'dist'
+    # dist_path.mkdir(parents=True)
+    text = ''
+    for file in assets_path.iterdir():
+        if file.suffix == '.txt':
+            with open(file, 'r', encoding='utf-8') as f:
+                text += f.read()
+                text += '\n'
+    analyzer = UDPipeAnalyzer()
+    analyzed = analyzer.analyze([text])
+
+    with open(dist_path / 'auto_annotated.conllu', 'w', encoding='utf-8') as file:
+        for i in analyzed:
+            file.write(i)
 
 
 if __name__ == "__main__":
