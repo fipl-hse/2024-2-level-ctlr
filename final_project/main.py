e11c39ec21bf3f87dcc3f9b188d5d9a81d4aded2
evgraff2005 <annaevgrafova22095@gmail.com>
2025-06-15 12:50:25 +0300

    trying to add .conllu to the artifacts

--- final_project/main.py
+++ final_project/main.py
@@ -12,29 +12,33 @@ def main() -> None:
     """
     Generate conllu file for provided corpus of texts.
     """
-    base_dir = Path(__file__).parent.parent
-    input_dir = base_dir / "final_project" / "assets" / "axmatova"
-    output_dir = base_dir / "final_project" / "dist"
+    project_dir = Path(__file__).parent
+    assets_dir = project_dir / "assets" / "axmatova"
+    output_dir = project_dir / "dist"
     output_file = output_dir / "auto_annotated.conllu"
 
-    texts = [txt.read_text(encoding="utf-8") for txt in sorted(input_dir.glob("*.txt"))]
-    combined_text = "\n".join(texts) + ("\n" if texts else "")
-    analyzed_data = UDPipeAnalyzer().analyze([combined_text])[0]
     output_dir.mkdir(parents=True, exist_ok=True)
-    output_file.write_text(analyzed_data, encoding="utf-8")
 
-    with open(output_file, "r", encoding="utf-8") as file:
-        conllu_data = file.read()
+    combined_text = ""
+    for txt_file in sorted(assets_dir.glob("*.txt")):
+        combined_text += txt_file.read_text(encoding="utf-8") + "\n"
+
+    analyzer = UDPipeAnalyzer()
+    conllu_data = analyzer.analyze([combined_text])[0]
+    output_file.write_text(conllu_data, encoding="utf-8")
+
     token_freq = {}
-    for line in conllu_data.split('\n'):
-        if line.strip() and not line.startswith('#'):
-            token = line.split('\t')[1]
+    for line in conllu_data.split("\n"):
+        if line.strip() and not line.startswith("#"):
+            token = line.split("\t")[1]
             token_freq[token] = token_freq.get(token, 0) + 1
 
-    with open(base_dir / "final_project" / "token_frequencies.json", "w", encoding="utf-8") as f:
-        json.dump(sorted(token_freq.items(),
-                         key=lambda x: x[1], reverse=False), f, ensure_ascii=False, indent=2)
-        f.write('\n')
+    with open(project_dir / "token_frequencies.json", "w", encoding="utf-8") as f:
+        json.dump(
+            sorted(token_freq.items(), key=lambda x: x[1], reverse=True),
+            f,
+            ensure_ascii=False,
+            indent=2,)
 
 
 if __name__ == "__main__":
