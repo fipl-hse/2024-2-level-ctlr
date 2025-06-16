efe5738addba6a82697c25365a703edf6d8e00a0
unknown <rjkzye2@mail.ru>
2025-06-17 00:19:25 +0300

    make PR

--- final_project/main.py
+++ final_project/main.py
@@ -2,14 +2,26 @@
 Final project implementation.
 """
 
+
 # pylint: disable=unused-import
 from pathlib import Path
+
+from lab_6_pipeline.pipeline import UDPipeAnalyzer
 
 
 def main() -> None:
     """
     Generate conllu file for provided corpus of texts.
     """
+    BUNIN = Path(__file__).parent / "assets/bunin"
+    RAW_CONLLU = Path(__file__).parent / "dist" / "auto_annotated.conllu"
+    all_txt = ""
+    for text in BUNIN.glob("*.txt"):
+        with open(text, "r", encoding="utf-8") as f:
+            all_txt += f.read() + "\n"
+    udpipe_analyzer = UDPipeAnalyzer()
+    with open(RAW_CONLLU, "w", encoding="utf-8") as f:
+        f.write(udpipe_analyzer.analyze([all_txt])[0])
 
 
 if __name__ == "__main__":
