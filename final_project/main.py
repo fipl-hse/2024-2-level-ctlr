2ee082e6b1408eeb1efebc5084afa50a13cf3347
dkole13 <dasha10206@gmail.com>
2025-06-16 14:42:37 +0300

    final touches

--- final_project/main.py
+++ final_project/main.py
@@ -2,16 +2,9 @@
 Final project implementation.
 """
 
-import json
-from collections import defaultdict
-
 # pylint: disable=unused-import
 from pathlib import Path
 
-import spacy_udpipe
-from spacy_conll.parser import ConllParser
-
-from core_utils.constants import PROJECT_ROOT
 from lab_6_pipeline.pipeline import UDPipeAnalyzer
 
 
@@ -30,32 +23,8 @@ def main() -> None:
     udpipe_analyzer = UDPipeAnalyzer()
     conllu = udpipe_analyzer.analyze([combined_text])[0]
     conllu_dir = Path(__file__).parent / "dist"
-    with open(conllu_dir / "auto_annotated.conllu", 'w', encoding='utf-8') \
-            as file:
+    with open(conllu_dir / "auto_annotated.conllu", 'w', encoding='utf-8') as file:
         file.write(str(conllu))
-
-    model_path = Path(PROJECT_ROOT) / "lab_6_pipeline" / \
-                 "assets" / "model" / "russian-syntagrus-ud-2.0-170801.udpipe"
-    model = spacy_udpipe.load_from_path(
-        lang='ru',
-        path=str(model_path)
-    )
-    model.add_pipe(
-        "conll_formatter",
-        last=True,
-        config={"conversion_maps": {"XPOS": {"": "_"}}, "include_headers": True},
-    )
-
-    conllu_doc = ConllParser(model).parse_conll_text_as_spacy(conllu.strip('\n'))
-    lemmas_frequencies = defaultdict(int)
-    for token in conllu_doc:
-        lemmas_frequencies[token.text.lower()] += 1
-    lemmas_frequencies_sorted = dict(sorted(lemmas_frequencies.items(), key=lambda item: item[1],
-                                            reverse=True))
-    with open(Path(__file__).parent / "data" / "tokens_frequencies.json", 'w',
-              encoding='utf-8') \
-            as file:
-        json.dump(lemmas_frequencies_sorted, file, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
