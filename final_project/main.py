ffeb2efeb72f6beb2fb775f526509abfb0b37b2c
unknown <rjkzye2@mail.ru>
2025-06-17 00:38:38 +0300

    change smth

--- final_project/main.py
+++ final_project/main.py
@@ -8,20 +8,20 @@ from pathlib import Path
 
 from lab_6_pipeline.pipeline import UDPipeAnalyzer
 
+BUNIN = Path(__file__).parent / "assets/bunin"
+RAW_CONLLU = Path(__file__).parent / "dist" / "auto_annotated.conllu"
 
 def main() -> None:
     """
     Generate conllu file for provided corpus of texts.
     """
-    BUNIN = Path(__file__).parent / "assets/bunin"
-    RAW_CONLLU = Path(__file__).parent / "dist" / "auto_annotated.conllu"
     all_txt = ""
     for text in BUNIN.glob("*.txt"):
         with open(text, "r", encoding="utf-8") as f:
             all_txt += f.read() + "\n"
     udpipe_analyzer = UDPipeAnalyzer()
     with open(RAW_CONLLU, "w", encoding="utf-8") as f:
-        f.write(udpipe_analyzer.analyze([all_txt])[0])
+        f.write(str(udpipe_analyzer.analyze([all_txt])[0]))
 
 
 if __name__ == "__main__":
